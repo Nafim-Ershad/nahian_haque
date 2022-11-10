e6ef900b49670cd1b3157d0372eedea237520d53
Nafim Ersahd <inan.nafim1089@outlook.com>
2022-11-11 01:32:55 +0600

    seperated Hero section as Js code

---
+++ components/hero.js
@@ -0,0 +1,26 @@
+export default function Hero() {
+    return `<section id="hero-container">
+        <div class="left-side">
+            <div class="name-title-container">
+                <a>NAHIAN HAQUE</a>
+                <div class="nav-bar">
+                    <i class="fa-solid fa-bars bars"></i>
+                </div>
+            </div>
+            <div class="hero-left">
+                <h1>NAHIAN FAIROOZ HAQUE</h1>
+            </div>
+        </div>
+        <div class="right-side">
+            <nav class="navigation-container">
+                <a>Works</a>
+                <a>Contact</a>
+                <a>About</a>
+            </nav>
+        </div>
+
+        <a href='#' class="hire-me-container">
+        Hire Me!
+        </a>
+    </section>`
+}
--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 import './style.scss';
 
+import Hero from './components/hero';
+
+document.getElementById("app").innerHTML = `
+    ${Hero()}
+`;
