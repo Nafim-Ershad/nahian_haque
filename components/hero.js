74ac6bff28d0f5aa25589bb43a429896139cf63f
Nafim Ersahd <inan.nafim1089@outlook.com>
2022-11-12 15:15:13 +0600

    tried to make it responsive

--- components/hero.js
+++ components/hero.js
@@ -1,14 +1,23 @@
 export default function Hero() {
     return `<section id="hero-container">
         <div class="left-side">
-            <div class="name-title-container">
+            <div class="header-container">
                 <a>NAHIAN HAQUE</a>
                 <div class="nav-bar">
                     <i class="fa-solid fa-bars bars"></i>
+                    <div class='nav-links-container'>
+                        <i class="fa-solid fa-xmark cancel"></i>
+                        <div class='nav-links'>
+                            <a>Works</a>
+                            <a>Contact</a>
+                            <a>About</a>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="hero-left">
                 <h1>NAHIAN FAIROOZ HAQUE</h1>
+                <h3>Call me a dud, but I'll make you CUM</h3>
             </div>
         </div>
         <div class="right-side">
